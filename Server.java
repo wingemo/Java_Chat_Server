cfe94f8464fdf3e255846bd559be80517020e591
Wingemo <philip.wingemo@outlook.com>
2022-06-08 21:53:45 +0200

    Update Server.java

--- Server.java
+++ Server.java
@@ -11,61 +11,61 @@ import java.util.concurrent.Executors;
  * Represents a server listening to a port and handles clients
  */
 public class Server implements Runnable {
-    private static String CLIENT_CONNECTED_MSG = "CLIENT CONNECTED: ";
-    private static String CLIENT_DISSCONNECTED_MSG = "CLIENT DISSCONNECTED: ";
-    private ConcurrentHashMap <SocketAddress, ClientHandler> clientMap;
-    private BlockingQueue<String> broadcastQueue;
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private Executor executor;
+  private static String CLIENT_CONNECTED_MSG = "CLIENT CONNECTED: ";
+  private static String CLIENT_DISSCONNECTED_MSG = "CLIENT DISSCONNECTED: ";
+  private ConcurrentHashMap < SocketAddress, ClientHandler > clientMap;
+  private BlockingQueue < String > broadcastQueue;
+  private ServerSocket serverSocket;
+  private Socket clientSocket;
+  private Executor executor;
 
-    /**
-     * Constructs a server with a specified number of threads and port
-     * @param port server port number
-     * @param threads specified number of threads
-     */
-    public Server(int port, int threads) throws IOException {
-        serverSocket = new ServerSocket(port);
-        clientMap = new ConcurrentHashMap < SocketAddress, ClientHandler >();
-        executor = Executors.newFixedThreadPool(threads);
-        this.executor.execute(new Broadcaster(clientMap, broadcastQueue));
-    }
+  /**
+   * Constructs a server with a specified number of threads and port
+   * @param port server port number
+   * @param threads specified number of threads
+   */
+  public Server(int port, int threads) throws IOException {
+    serverSocket = new ServerSocket(port);
+    clientMap = new ConcurrentHashMap < SocketAddress, ClientHandler > ();
+    executor = Executors.newFixedThreadPool(threads);
+    this.executor.execute(new Broadcaster(clientMap, broadcastQueue));
+  }
 
-    /**
-     * Adds client to ConcurrentHashMap
-     * @param socket client socket
-     */
-    private void addClient(Socket socket) {
-        this.broadcast(CLIENT_CONNECTED_MSG + socket.getRemoteSocketAddress());
-        this.clientMap.put(socket.getRemoteSocketAddress(), new ClientHandler(socket));
-    }
+  /**
+   * Adds client to ConcurrentHashMap
+   * @param socket client socket
+   */
+  private void addClient(Socket socket) {
+    this.broadcast(CLIENT_CONNECTED_MSG + socket.getRemoteSocketAddress());
+    this.clientMap.put(socket.getRemoteSocketAddress(), new ClientHandler(socket));
+  }
 
-    /**
-     * Remove client from ConcurrentHashMap
-     * @param client client socket
-     */
-    public void removeClient(ClientHandler client) {
-        this.clientMap.remove(client.getSocket().getRemoteSocketAddress());
-        this.broadcast(CLIENT_DISSCONNECTED_MSG + client.getSocket().getRemoteSocketAddress());
-    }
+  /**
+   * Remove client from ConcurrentHashMap
+   * @param client client socket
+   */
+  public void removeClient(ClientHandler client) {
+    this.clientMap.remove(client.getSocket().getRemoteSocketAddress());
+    this.broadcast(CLIENT_DISSCONNECTED_MSG + client.getSocket().getRemoteSocketAddress());
+  }
 
-    public void broadcast(String message) {
-        try {
-            broadcastQueue.put(message);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+  public void broadcast(String message) {
+    try {
+      broadcastQueue.put(message);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void run() {
-        try {
-            while (true) {
-                this.clientSocket = serverSocket.accept();
-                addClient(clientSocket);
-            }
-        } catch (Exception exception) {
-            System.out.println(exception);
-        }
+  @Override
+  public void run() {
+    try {
+      while (true) {
+        this.clientSocket = serverSocket.accept();
+        addClient(clientSocket);
+      }
+    } catch (Exception exception) {
+      System.out.println(exception);
     }
+  }
 }
