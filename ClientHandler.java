9ea51ddfe0510315861b07b96a88337556514505
Wingemo <philip.wingemo@outlook.com>
2022-06-08 21:40:25 +0200

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -4,9 +4,35 @@ public class ClientHandler implements Runnable {
 
     private Server server;
 
+    private Socket socket;
+
     private int identifier;
 
     public ClientHandler(Socket clientSocket) {
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
     }
 
     @Override
@@ -14,3 +40,4 @@ public class ClientHandler implements Runnable {
 
     }
 }
+
