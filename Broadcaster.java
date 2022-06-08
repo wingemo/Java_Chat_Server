84e22852aa7df55f9c836ab5eba5dae146b73466
Wingemo <philip.wingemo@outlook.com>
2022-06-08 21:39:25 +0200

    Update Broadcaster.java

--- Broadcaster.java
+++ Broadcaster.java
@@ -3,33 +3,31 @@ import java.util.Random;
 
 class Broadcaster implements Runnable {
 
-        private BlockingQueue<String message> broadcastQueue;
-        private ConcurrentHashMap <SocketAddress, ClientHandler> clientMap;
+  private BlockingQueue < String message > broadcastQueue;
+  private ConcurrentHashMap < SocketAddress, ClientHandler > clientMap;
 
-	Consumer(BlockingQueue<Task> queue) { 
-		this.queue = queue; 
-	}
-	
-	public void run() {
-		while (true) {
-			try {
-				String message = queue.take();
-				consume(task);
-			}
-			catch (Exception exception) {
-				System.out.println(exception);
-			}
-		}
-	}
-	
-	void consume(Task task) {
-		try {
-			clientMap.forEach((id, client) -> {
-                           client.send(messeage);
-                        });
-		}
-		catch (Exception exception) {
-			System.out.println(exception);
-		}
-	}
+  Broadcaster(BlockingQueue < Task > queue) {
+    this.queue = queue;
+  }
+
+  public void run() {
+    while (true) {
+      try {
+        String message = queue.take();
+        consume(task);
+      } catch (Exception exception) {
+        System.out.println(exception);
+      }
+    }
+  }
+
+  private void consume(String message) {
+    try {
+      clientMap.forEach((id, client) -> {
+        client.send(messeage);
+      });
+    } catch (Exception exception) {
+      System.out.println(exception);
+    }
+  }
 }
