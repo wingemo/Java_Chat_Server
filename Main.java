ec14c563af3517be88b4bd03800ea9abbfc21b8d
Wingemo <philip.wingemo@outlook.com>
2022-06-09 09:14:25 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 import java.io.IOException;
 
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Zara Ali
+* @version 1.0
+* @since   2014-03-31 
+*/
 public class Main {
   private static Server server;
   private static int threads;
